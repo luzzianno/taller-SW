f682707f989040712bfdbfe42ad779eae017b310
MiiGuii <lucianorichard0@gmail.com>
2018-11-19 18:27:31 -0300

    bootstrap4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { NgbModule,NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,7 +30,9 @@ TagInputModule.withDefaults({
   ],
   imports: [
     TagInputModule,
+    NgbPaginationModule,
     BrowserAnimationsModule,
+    NgbModule,
     ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule,
